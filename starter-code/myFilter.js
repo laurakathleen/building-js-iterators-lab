ab8297a215903faeb1e2d679d979bbb1db805e69
laurakathleen <laurakwalley@gmail.com>
2016-08-22 12:46:59 -0700

    edits myFilter to break down the filter() function

--- starter-code/myFilter.js
+++ starter-code/myFilter.js
@@ -1,8 +1,15 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Filter
 function myFilter(arr, callback) {
-
-//  CODE INSIDE HERE   //
-
+//create a new array to store your new filtered values within:
+var newArr = [];
+//loop through the original array to check the value at each index:
+for (var i=0; i<arr.length; i++){
+	//run the callback function on the indeces
+	if (callback(arr[i], i, arr)){
+		//If the indeces run through the function are true, add that to the new array;
+		newArr.push(arr[i]);
+	}
+	return newArr;
 }
 
 
