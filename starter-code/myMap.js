81ac4467138b80e4f037b254151c733c4b3861c7
laurakathleen <laurakwalley@gmail.com>
2016-08-22 12:27:12 -0700

    edits myMap to break down map function

--- starter-code/myMap.js
+++ starter-code/myMap.js
@@ -1,9 +1,15 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Map
 
 function myMap(arr, callback) {
-
-//  CODE INSIDE HERE   //
-
+	//create a new array to hold new data:
+	var newArr= [];
+	//loop through the length of the original array:
+	for (var i=0; i<arr.length; i++){
+		//use the callback function of the current index, index, and array to push (to add) new data to your new array:
+		 newArr.push(callback(arr[i], i, arr));
+	}
+	//return new array to see your new data:
+	return newArr;
 }
 
 
